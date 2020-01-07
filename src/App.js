b068d2e552bea180bbeb62d785a3219a0e6dec43
Sami Beig <samibeig@Samis-MacBook-Pro.local>
2020-01-07 13:40:52 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import ReactDOM from 'react-dom';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class CreateAllCards extends Component {
+  render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  
+  <div><ContactCard name="Tom" mobileNumber='(646)-929-2049' workNumber='323-425-9491' email="tomford@gmail.com"/> 
+  
+  <ContactCard name="Nick" mobileNumber='(123)-456-7811' workNumber='N/A' email="NickWright@gmail.com"/> 
+  
+  <ContactCard name="Ross" mobileNumber='(198)-765-4311' workNumber='92901930491' email="RossRick@gmail.com"/> 
+
+  </div>
+  )
+  }
+  
+
+};
+
+//Contact card component
+class ContactCard extends Component{
+  render(){
+    return <div> <p>Contact Card</p> <ul> <li>{this.props.name}</li> <li>{this.props.email}</li> <li>{this.props.mobileNumber}</li> <li>{this.props.workNumber}</li></ul></div>
+  }
+};
+
+ContactCard.propTypes = {
+  name: PropTypes.string.isRequired,
+  mobileNumber: PropTypes.string.isRequired,
+  workNumber: PropTypes.string,
+  email: PropTypes.string.isRequired
 }
 
-export default App;
+//ReactDOM.render(<App/>);
+//ReactDOM.render(<createTom />, document.getElementById('tom');
+//ReactDOM.render(<createNick />, document.getElementById('nick'));
+//ReactDOM.render(<createAllCards/>, document.getElementById("tom"))
+
+export {CreateAllCards, ContactCard} ;
